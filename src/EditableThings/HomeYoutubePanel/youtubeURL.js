085fd900c2704817695dc186f1cdb2c41ccb38ff
0stam <romanskiba0@gmail.com>
2026-02-17 13:25:17 +0100

    Update YT url

--- src/EditableThings/HomeYoutubePanel/youtubeURL.js
+++ src/EditableThings/HomeYoutubePanel/youtubeURL.js
@@ -1,1 +1,1 @@
-export const youtube_src = "https://www.youtube.com/embed/tIfBVFOW6Bk?si=htVKKXPA_yICHA-H"
+export const youtube_src = "https://www.youtube.com/embed/FRsIe-OoU7E?si=DhLOdx2lziEt13Ha"
